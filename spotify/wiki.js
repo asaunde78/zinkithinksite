1aa33951fdfcdf3ab63df0d9e8ea1d826b038ebe
asaunde78 <asaunde78@gmail.com>
2024-05-31 22:55:49 +0000

    update the data api to be music brainz instead of wikipedia (much better)

--- spotify/wiki.js
+++ spotify/wiki.js
@@ -4,7 +4,7 @@ const button = document.getElementById("sendbutton")
 const spotifybutton = document.getElementById("spotifybutton")
 const input = document.getElementById("textinput")
 const wikiEndpoint = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=categories&generator=search&gsrlimit=5&cllimit=1000&gsrsearch="
-
+const mbzendpoint = "http://musicbrainz.org/ws/2/artist/?fmt=json&limit=1&query="
 
 spotifybutton.addEventListener("click", async () => {
     const response = await fetch("/api/spotify/likes")
@@ -21,49 +21,15 @@ spotifybutton.addEventListener("click", async () => {
                 complete[artist.name].count += 1
                 continue
             }
-            const res = await fetch(wikiEndpoint + encodeURIComponent(artist.name))
+            const res = await fetch(mbzendpoint + encodeURIComponent(artist.name))
             const d = await res.json()
             console.log(artist.name)
             
             complete[artist.name] = {count:1}
             if(d ){
-                // console.log(d)
-                const articles = Object.values(d?.query?.pages || {}).sort(  (a,b) => {
-                    // console.log(a, b);
-                    return a.index < b.index ? -1 : 1
-                })
-                complete[artist.name].data = articles[0].title
-                // complete[artist.name].data = articles.map( (li) => {
-                //     let musician_score = 0
-                //     let band_score = 0
-                //     for(const cat of li.categories) {
-                //         let musicianmatches = cat.title.match(/singer|musician|artist/g)
-                //         let bandmatches = cat.title.match(/music group|group|band|supergroup/g)
-                        
-                //         if(musicianmatches != null) {
-                //             musician_score += musicianmatches.length
-                //         }
-                //         if(bandmatches != null) {
-                //             band_score += bandmatches.length
-                //         }
-
-                //     }
-
-                //     // return {musician_score:li.categories.map( (cat) => cat.title)
-                //     //     .reduce( (acc, curr) => {
-                //     //         // console.log(curr.match(/singer|musician|artist/g));
-                //     //         let matches = curr.match(/singer|musician|artist/g)
-                //     //         // console.log(matches == null)
-                //     //         let out = 0
-                //     //         if(matches != null) {
-                //     //             out = matches.length
-                //     //         }
-                //     //         return acc + out}, 0), title:li.title
-                //     //     }
-                //     // }
-                //     return {musician_score:musician_score, band_score, title:li.title}
-                //     }
-                // )
+                
+                complete[artist.name].data = d.artists[0]
+                
             }
             
             
@@ -77,23 +43,10 @@ spotifybutton.addEventListener("click", async () => {
 button.addEventListener("click", async() => {
     const searchTerm = input.value
     try {
-        var response = await fetch(wikiEndpoint + encodeURIComponent(searchTerm))
+        var response = await fetch(mbzendpoint + encodeURIComponent(searchTerm))
         var data = await response.json()
-        // console.log(Object.values(data?.query?.pages))
-        const articles = Object.values(data?.query?.pages).sort(  (a,b) => {
-            // console.log(a, b);
-            return a.index < b.index ? -1 : 1
-        })
-        console.log(articles)
-        // const titles = articles.map( (a) => encodeURIComponent(a.title))
-        // const titlesearch = titles.join("|")
-        // console.log(titles)
-        // console.log("title", titlesearch)
-        // const second_endpoint = `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&titles=${titlesearch}&gsrlimit=10`
-
-        // response = await fetch(second_endpoint)
-        // data = await response.json()
-        // console.log(data)
+        console.log(data)
+        
     }
     catch(error) {
         console.log("error", error)
